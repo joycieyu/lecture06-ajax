baf15f0183bbde0ed6c4d59665e16ad2554c062d
Joycie Yu <joycie@uw.edu>
2016-10-24 12:24:58 -0700

    complete lecture activities

--- js/app.js
+++ js/app.js
@@ -19,24 +19,29 @@ var songs = [{
  * This function takes in an object representing a SINGLE music track
  * and displays it on the page.
  */
+
+var nowPlaying = new Audio();
+
 function renderTrack(track){
 
   //select the `.records` section that will contain the records
   //you could also do this before the function declaration
 
-
+var records = document.querySelector('.records');
   //create a new `img` element to visually represent the track
 
-
+var record = document.createElement('img');
   //set the image's `src` to be the track's `album_image`
 
-
+record.setAttribute('src', track.album.images[0].url);
+//record.setAttribute('src',track.album_image);
   //always give images `alt` attributes! The `name` of the track will work fine
 
-
+record.setAttribute('alt', track.name);
+record.setAttribute('title', track.name); //for mouse-over descriptions
   //attach the image element to the `.records` section
 
-
+records.appendChild(record);
   //add an event listener so that when the track is clicked on,
   //it starts to spin! (add the Font-Awesome utility class `fa-spin`)
   //
@@ -44,10 +49,52 @@ function renderTrack(track){
   //to change the element that was clicked on!
   //
   //Bonus: can you make the track stop spinning when clicked again?
+  record.addEventListener('click', function(event){
+    var whichRecord = event.target;
 
-
+    //clicked the current song
+    if(nowPlaying.src === track.preview_url){
+      if (nowPlaying.paused) {
+        nowPlaying.play();
+      } else {
+        nowPlaying.pause();
+      }
+    }
+    else { //something different 
+      nowPlaying.pause();
+      nowPlaying = new Audio(track.preview_url);
+      nowPlaying.play();
+    }
+    whichRecord.classList.toggle('fa-spin');
+  })
 } //end method
 
 //call this function `forEach` element in the `songs` array!
 
+//songs.forEach(renderTrack);
 
+var form= document.querySelector('form');
+var input = document.querySelector('#searchQuery');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  console.log("Submitted!");
+
+  //do my thing
+  var userInput = input.value;
+  var url ='https://api.spotify.com/v1/search?type=track&q='+userInput;
+  console.log("fetching from", url);
+
+  fetch(url)
+    .then(function(response) {
+      var newPromise = response.json();
+      return newPromise;
+    })
+    .then(function(data){
+    console.log(data);
+    data.tracks.items.forEach(renderTrack);
+  })
+
+console.log("after fetch");
+  return false
+});
